115af497797d9ea106d625cd52810a52ad203afd
kayak <kayak@kayak.rocks>
2020-06-06 18:24:13 +1000

    ✨ Match Everything But Letters and Numbers
    JS > Regular Expressions > Match Everything But Letters and Numbers
    
    closes #21

---
+++ JSAlgosAndDataStrs/RegEx/matchAllButLettersAndNumbers.js
@@ -0,0 +1,5 @@
+let quoteSample = "The five boxing wizards jump quickly.";
+let nonAlphabetRegex = /\W/g; 
+let result = quoteSample.match(nonAlphabetRegex).length;
+
+module.exports = nonAlphabetRegex
