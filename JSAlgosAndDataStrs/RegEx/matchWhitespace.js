9fe95268d95c78de1784d12bb8122c78a4213aab
kayak <kayak@kayak.rocks>
2020-06-07 12:02:50 +1000

    ✨ Match Whitespace
    JS > Regular Expressions > Match Whitespace
    
    closes #26

---
+++ JSAlgosAndDataStrs/RegEx/matchWhitespace.js
@@ -0,0 +1,5 @@
+let sample = 'Whitespace is important in separating words'
+let countWhiteSpace = /\s/g
+let result = sample.match(countWhiteSpace)
+
+module.exports = countWhiteSpace
---
+++ JSAlgosAndDataStrs/RegEx/matchWhitespace.test.js
@@ -0,0 +1,17 @@
+const regex = require('./matchWhitespace.js')
+
+describe(`Regular Expressions: Match Whitespace`, () => {
+  test(`Your regex should find eight spaces in "Men are from Mars and women are from Venus."`, () => {
+    const string = 'Men are from Mars and women are from Venus.'
+    expect(string.match(regex).length).toBe(8)
+  })
+
+  test(`Your regex should find three spaces in "Space: the final frontier."`, () => {
+    const string = 'Space: the final frontier.'
+    expect(string.match(regex).length).toBe(3)
+  })
+
+  test(`Your regex should find no spaces in "MindYourPersonalSpace"`, () => {
+    expect('MindYourPersonalSpace'.match(regex)).toBe(null)
+  })
+})
