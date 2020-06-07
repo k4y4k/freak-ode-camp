3dd41b133e2914e988198e56ecc3829389ebde06
kayak <kayak@kayak.rocks>
2020-06-07 13:12:51 +1000

    ✨ Specify Exact Number of Matches
    JS > Regular Expressions > Specify Exact Number of Matches
    
    closes #35

---
+++ JSAlgosAndDataStrs/RegEx/specifyExactNumberOfMatches.test.js
@@ -0,0 +1,29 @@
+const regex = require('./specifyExactNumberOfMatches.js')
+
+describe(`Regular Expressions: Specify Exact Number of Matches`, () => {
+  test(`The regex should not match "Timber"`, () => {
+    const string = 'Timber'
+    expect(string.match(regex)).toBe(null)
+  })
+
+  test(`The regex should not match "Timmber"`, () => {
+    const string = 'Timmber'
+    expect(string.match(regex)).toBe(null)
+  })
+
+  test(`The regex should not match "Timmmber"`, () => {
+    const string = 'Timmmber'
+    expect(string.match(regex)).toBe(null)
+  })
+
+  test(`The regex should not match "Timber" with 30 m's in it.`, () => {
+    const string = 'Ti' + 'm'.repeat(30) + 'ber'
+    expect(string.match(regex)).toBe(null)
+  })
+
+  test(`The regex should match "Timmmmber"`, () => {
+    const string = 'Timmmmber'
+
+    expect(string.match(regex)).toEqual(expect.arrayContaining([string]))
+  })
+})
