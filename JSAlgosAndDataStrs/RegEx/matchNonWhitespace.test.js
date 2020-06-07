e3f435ec1b7b2ee61878967fd8351d5f453c845d
kayak <kayak@kayak.rocks>
2020-06-07 12:29:35 +1000

    ✨ Match Non-Whitespace Characters
    JS > Regular Expressions > Match Non-Whitespace Characters
    
    closes #28

---
+++ JSAlgosAndDataStrs/RegEx/matchNonWhitespace.test.js
@@ -0,0 +1,17 @@
+const regex = require('./matchNonWhitespace.js')
+
+describe(`Regular Expressions: Match Non-Whitespace Characters`, () => {
+  test(`Your regex should find 35 non-spaces in "Men are from Mars and women are from Venus."`, () => {
+    const string = 'Men are from Mars and women are from Venus.'
+    expect(string.match(regex).length).toBe(35)
+  })
+
+  test(`Your regex should find 23 non-spaces in "Space: the final frontier."`, () => {
+    const string = 'Space: the final frontier.'
+    expect(string.match(regex).length).toBe(23)
+  })
+
+  test(`Your regex should find 21 non-spaces in "MindYourPersonalSpace"`, () => {
+    expect('MindYourPersonalSpace'.match(regex).length).toBe(21)
+  })
+})
