64d4decfa49a986b7f423c121c0490bbc46c662f
kayak <kayak@kayak.rocks>
2020-06-09 14:12:28 +1000

    🏗 Updating backlog
    these lessons were done before I started this repo
    
    closes #122, closes #123, closes #124, closes #125, closes #126, closes #127, closes #128, closes #129, closes #130, closes #131, closes #132, closes #133, closes #134, closes #135, closes #136, closes #137, closes #138, closes #139, closes #140, closes #141, closes #142, closes #143, closes #144, closes #145, closes #146, closes #147, closes #148, closes #149, closes #150, closes #151, closes #152, closes #153, closes #154, closes #155, closes #156, closes #157, closes #158, closes #159, closes #160, closes #161, closes #162, closes #163, closes #164, closes #165, closes #166, closes #167, closes #168, closes #169, closes #170, closes #171, closes #172, closes #173, closes #174, closes #175, closes #176, closes #177, closes #178, closes #179, closes #180, closes #181, closes #182, closes #183, closes #184, closes #185, closes #186, closes #187, closes #188, closes #189, closes #190, closes #191, closes #192, closes #193, closes #194, closes #195, closes #196, closes #197, closes #198, closes #199, closes #200, closes #201, closes #202, closes #203, closes #204, closes #205, closes #206, closes #207, closes #208, closes #209, closes #210, closes #211, closes #212, closes #213, closes #214, closes #215, closes #216, closes #217, closes #218, closes #219, closes #220, closes #221, closes #222, closes #223, closes #224, closes #225, closes #226, closes #227, closes #228, closes #229, closes #230, closes #231, closes #232, and closes #233.

--- makeIssues.js
+++ makeIssues.js
@@ -1,52 +1,44 @@
 const { exec } = require('child_process')
 
-const arr = [
-  'Using the Test Method',
-  'Match Literal Strings',
-  'Match a Literal String with Different Possibilities',
-  'Ignore Case While Matching',
-  'Extract Matches',
-  'Find More Than the First Match',
-  'Match Anything with Wildcard Period',
-  'Match Single Character with Multiple Possibilities',
-  'Match Letters of the Alphabet',
-  'Match Numbers and Letters of the Alphabet',
-  'Match Single Characters Not Specified',
-  'Match Characters that Occur One or More Times',
-  'Match Characters that Occur Zero or More Times',
-  'Find Characters with Lazy Matching',
-  'Find One or More Criminals in a Hunt',
-  'Match Beginning String Patterns',
-  'Match Ending String Patterns',
-  'Match All Letters and Numbers',
-  'Match Everything But Letters and Numbers',
-  'Match All Numbers',
-  'Match All Non-Numbers',
-  'Restrict Possible Usernames',
-  'Match Whitespace',
-  'Match Non-Whitespace Characters',
-  'Specify Upper and Lower Number of Matches',
-  'Specify Only the Lower Number of Matches',
-  'Specify Exact Number of Matches',
-  'Check for All or None',
-  'Positive and Negative Lookahead',
-  'Check For Mixed Grouping of Characters',
-  'Reuse Patterns Using Capture Groups',
-  'Use Capture Groups to Search and Replace',
-  'Remove Whitespace from Start and End',
-]
+const arr = []
 
-for (let i = 0; i < arr.length; i++) {
-  exec(`ghi open "${arr[i]}" -M 1`, (err, stout, sterr) => {
-    if (err) {
-      console.log(err)
-      return
-    }
+const openIssues = () => {
+  for (let i = 0; i < arr.length; i++) {
+    exec(
+      `ghi open "${arr[i]}" --claim -L "Basic JavaScript" -M 1`,
 
-    if (sterr) {
-      console.log(sterr)
-      return
-    }
-    console.log(stout)
-  })
+      (err, stout, sterr) => {
+        if (err) {
+          console.log(err)
+          return
+        }
+
+        if (sterr) {
+          console.log(sterr)
+          return
+        }
+        console.log(stout)
+      }
+    )
+  }
 }
+
+const issueIds = { from: 122, to: 233 }
+
+const closeIssues = () => {
+  // generate closing commit message - GitHub can process the closures on their
+  // end, instead of ratelimiting me.
+  let message = ''
+
+  for (let i = issueIds.from; i < issueIds.to; i++) {
+    message += `closes #${i}, `
+  }
+
+  // and, because I care about grammar (but not commit message conventions apparently)
+
+  message += `and closes #${issueIds.to}.`
+
+  console.log(message)
+}
+
+closeIssues()
