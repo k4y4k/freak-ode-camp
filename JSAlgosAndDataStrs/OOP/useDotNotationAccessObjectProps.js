4c62abfe40156ea724470821231ad31af2320a5c
kayak <kayak@kayak.rocks>
2020-06-12 15:09:02 +1000

    ✨ Use Dot Notation to Access Object Properties
    JS > Object Oriented Programming > Use Dot Notation to Access the Properties of an Object
    
    closes #47

---
+++ JSAlgosAndDataStrs/OOP/useDotNotationAccessObjectProps.js
@@ -0,0 +1,9 @@
+let dog = {
+  name: 'Spot',
+  numLegs: 4,
+}
+// Only change code below this line
+const logName = console.log(dog.name)
+const logLegs = console.log(dog.numLegs)
+
+module.exports = { logName, logLegs, dog }
