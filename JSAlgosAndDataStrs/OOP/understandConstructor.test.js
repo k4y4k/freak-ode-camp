bd109a6af180d128b8658689cdcd8244c343e0b0
kayak <kayak@kayak.rocks>
2020-07-24 14:16:59 +1000

    ✨ Understand the Constructor Property
    JS > Object Oriented Programming > Understand the Constructor Property
    
    closes #54

---
+++ JSAlgosAndDataStrs/OOP/understandConstructor.test.js
@@ -0,0 +1,18 @@
+const {
+  Dog,
+  joinDogFraternity,
+  patti,
+  barkbark,
+} = require('./understandConstructor.js')
+
+describe(`Object Oriented Programming: Understand the Constructor Property`, () => {
+  test(`joinDogFraternity should be a function`, () => {
+    expect(joinDogFraternity).toBeFunction()
+  })
+
+  test(`joinDogFraternity should return true if candidate is an instance of Dog`, () => {
+    expect(patti.constructor === Dog).toBeTruthy()
+    expect(joinDogFraternity(patti)).toBeTruthy()
+    expect(joinDogFraternity(barkbark)).toBeFalsy()
+  })
+})
