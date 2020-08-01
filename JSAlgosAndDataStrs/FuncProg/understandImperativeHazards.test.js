acb1eaf9e2fccc944960da3a43bf5a01d48b6fab
kayak <kayak@kayak.rocks>
2020-08-01 17:48:29 +1000

    ✨ Understand the Hazards of Imperative Code
    JS > Functional Programming > Understand the Hazards of Using Imperative Code
    
    closes #73

---
+++ JSAlgosAndDataStrs/FuncProg/understandImperativeHazards.test.js
@@ -0,0 +1,22 @@
+const finalTabs = require('./understandImperativeHazards.js')
+
+describe(`Functional Programming: Understand the Hazards of Using Imperative Code`, () => {
+  test(`finalTabs.tabs should be ['FB', 'Gitter', 'Reddit', 'Twitter', 'Medium', 'new tab', 'Netflix', 'YouTube', 'Vine', 'GMail', 'Work mail', 'Docs', 'freeCodeCamp', 'new tab']`, () => {
+    expect(finalTabs.tabs).toStrictEqual([
+      'FB',
+      'Gitter',
+      'Reddit',
+      'Twitter',
+      'Medium',
+      'new tab',
+      'Netflix',
+      'YouTube',
+      'Vine',
+      'GMail',
+      'Work mail',
+      'Docs',
+      'freeCodeCamp',
+      'new tab',
+    ])
+  })
+})
