0e018314d8343bd80da46f4f45ba22ed8a0c6b21
kayak <kayak@kayak.rocks>
2020-08-01 12:11:17 +1000

    ✨ Learn About Functional Programming
    JS > Functional Programming > Learn About Functional Programming
    
    closes #70

---
+++ JSAlgosAndDataStrs/FuncProg/learnAboutFuncProgramming.test.js
@@ -0,0 +1,12 @@
+const tea4TeamFCC = require('./learnAboutFuncProgramming.js')
+
+describe(`Functional Programming: Learn About Functional Programming`, () => {
+  test(`The tea4TeamFCC variable should hold 40 cups of tea for the team.`, () => {
+    expect(tea4TeamFCC.length).toBe(40)
+  })
+
+  test(`The tea4TeamFCC variable should hold cups of green tea.`, () => {
+    const hasGreenTea = el => el === 'greenTea'
+    expect(tea4TeamFCC).toSatisfyAll(hasGreenTea)
+  })
+})
