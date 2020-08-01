89e262287cd568b523f72ce43de4e85496641075
kayak <kayak@kayak.rocks>
2020-08-01 15:20:58 +1000

    ✨ Understand Functional Programming Terminology
    JS > Functional Programming > Understand Functional Programming Terminology
    
    closes #72

---
+++ JSAlgosAndDataStrs/FuncProg/understandTerminology.test.js
@@ -0,0 +1,26 @@
+const {
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC,
+} = require('./understandTerminology.js')
+
+describe(`Functional Programming: Understand Functional Programming Terminology`, () => {
+  test(`The tea4GreenTeamFCC variable should hold 27 cups of green tea for the team`, () => {
+    expect(tea4GreenTeamFCC.length).toBe(27)
+  })
+
+  test(`The tea4GreenTeamFCC variable should hold cups of green tea.`, () => {
+    const hasGreenTea = el => el === 'greenTea'
+
+    expect(tea4GreenTeamFCC).toSatisfyAll(hasGreenTea)
+  })
+
+  test(`The tea4BlackTeamFCC variable should hold 13 cups of black tea.`, () => {
+    expect(tea4BlackTeamFCC.length).toBe(13)
+  })
+
+  test(`The tea4BlackTeamFCC variable should hold cups of black tea.`, () => {
+    const hasBlackTea = el => el === 'blackTea'
+
+    expect(tea4BlackTeamFCC).toSatisfyAll(hasBlackTea)
+  })
+})
