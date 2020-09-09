2377d304d5aef531cc824ff4cd5f9979285cb9bd
kayak <kayak@kayak.rocks>
2020-09-09 13:51:40 +1000

    ✨ Implement map On a Prototype
    JS > Functional Programming > Implement map on a Prototype
    
    closes #77

---
+++ JSAlgosAndDataStrs/FuncProg/implementMapOnAPrototype.js
@@ -0,0 +1,20 @@
+/* eslint-disable func-names, no-extend-native, camelcase */
+
+// The global variable
+const s = [23, 65, 98, 5]
+
+Array.prototype.myMap = function (callback) {
+  const newArray = []
+
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]))
+  }
+
+  return newArray
+}
+
+const new_s = s.myMap(function (item) {
+  return item * 2
+})
+
+module.exports = { new_s }
