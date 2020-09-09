1bbfc965f4219188137a605237bcf2d4e7d01aa3
kayak <kayak@kayak.rocks>
2020-09-09 15:56:44 +1000

    ✨ Implement filter() on a Prototype
    JS > Functional Programming > Implement the filter Method on a Prototype
    
    closes #78

---
+++ JSAlgosAndDataStrs/FuncProg/implementFilterOnAPrototype.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable camelcase */
+
+const { new_s } = require('./implementFilterOnAPrototype.js')
+
+describe('Functional Programming: Implement the filter Method on a Prototype', () => {
+  test('new_s should equal [23, 65, 5].', () => {
+    expect(new_s).toStrictEqual([23, 65, 5])
+  })
+})
