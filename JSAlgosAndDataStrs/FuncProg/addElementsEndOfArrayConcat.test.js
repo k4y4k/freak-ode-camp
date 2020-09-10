68897bc1de50dce013c19e61e3acaf990d17070f
kayak <kayak@kayak.rocks>
2020-09-10 16:53:05 +1000

    ✨ Add Elements to the End of an Array Using concat
    JS > Functional Programming > Add Elements to the End of an Array Using
    concat Instead of push
    
    closes #83

---
+++ JSAlgosAndDataStrs/FuncProg/addElementsEndOfArrayConcat.test.js
@@ -0,0 +1,27 @@
+const {
+  first,
+  second,
+  nonMutatingPush,
+} = require('./addElementsEndOfArrayConcat.js')
+
+describe('Functional Programming: Add Elements to the End of an Array Using concat Instead of push', () => {
+  test('The first array should not change.', () => {
+    const firstOrig = [1, 2, 3]
+
+    expect(first).toBeArray()
+    expect(first).toStrictEqual(firstOrig)
+  })
+
+  test('The second array should not change', () => {
+    const secondOrig = [4, 5]
+
+    expect(second).toBeArray()
+    expect(second).toStrictEqual(secondOrig)
+  })
+
+  test('nonMutatingPush([1, 2, 3], [4, 5]) should return [1, 2, 3, 4, 5].', () => {
+    const res = nonMutatingPush([1, 2, 3], [4, 5])
+
+    expect(res).toStrictEqual([1, 2, 3, 4, 5])
+  })
+})
