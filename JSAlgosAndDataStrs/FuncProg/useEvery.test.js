6006aa0e07fe987b0b4fa6955f27dd4013c1dcac
kayak <kayak@kayak.rocks>
2020-09-11 16:36:18 +1000

    ✨ Use every() to Check an Array Meets Criteria
    JS > Functional Programming > Use the every Method to Check that Every
    Element in an Array Meets a Criteria
    
    closes #92

---
+++ JSAlgosAndDataStrs/FuncProg/useEvery.test.js
@@ -0,0 +1,21 @@
+const checkPositive = require('./useEvery.js')
+
+describe('Functional Programming: Use the every Method to Check that Every Element in an Array Meets a Criteria', () => {
+  test('checkPositive([1, 2, 3, -4, 5]) should return false.', () => {
+    const res = checkPositive([1, 2, 3, -4])
+
+    expect(res).toBeFalse()
+  })
+
+  test('checkPositive([1, 2, 3, 4, 5]) should return true.', () => {
+    const res = checkPositive([1, 2, 3, 4, 5])
+
+    expect(res).toBeTrue()
+  })
+
+  test('checkPositive([1, -2, 3, -4, 5]) should return false.', () => {
+    const res = checkPositive([1, -2, 3, -4, 5])
+
+    expect(res).toBeFalse()
+  })
+})
