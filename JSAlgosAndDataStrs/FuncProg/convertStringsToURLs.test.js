ecd4287a566a5b86c5f4386bd7e85ddf5eb0a849
kayak <kayak@kayak.rocks>
2020-09-11 16:23:47 +1000

    ✨ Convert Strings to URL Slugs
    JS > Functional Programming > Apply Functional Programming to Convert
    Strings to URL Slugs
    
    closes #86

---
+++ JSAlgosAndDataStrs/FuncProg/convertStringsToURLs.test.js
@@ -0,0 +1,27 @@
+const urlSlug = require('./convertStringsToURLs.js')
+
+describe('Functional Programming: Apply Functional Programming to Convert Strings to URL Slugs', () => {
+  test('urlSlug("Winter Is Coming") should return "winter-is-coming".', () => {
+    const res = urlSlug('Winter Is Coming')
+
+    expect(res).toBe('winter-is-coming')
+  })
+
+  test('urlSlug(" Winter Is  Coming") should return "winter-is-coming".', () => {
+    const res = urlSlug(' Winter Is  Coming')
+
+    expect(res).toBe('winter-is-coming')
+  })
+
+  test('urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone") should return "a-mind-needs-books-like-a-sword-needs-a-whetstone".', () => {
+    const res = urlSlug('A Mind Needs Books Like A Sword Needs A Whetstone')
+
+    expect(res).toBe('a-mind-needs-books-like-a-sword-needs-a-whetstone')
+  })
+
+  test('urlSlug("Hold The Door") should return "hold-the-door"', () => {
+    const res = urlSlug('Hold The Door')
+
+    expect(res).toBe('hold-the-door')
+  })
+})
