48b39e8ba1f5fa042780209cc75cb14b1c6bb0ae
kayak <kayak@kayak.rocks>
2020-09-27 15:27:34 +1000

    ✨ Sorted Union
    JS > Intermediate Algorithm Scripting > Sorted Union
    
    closes #99

---
+++ JSAlgosAndDataStrs/IntermediateScripting/sortedUnion.test.js
@@ -0,0 +1,23 @@
+const uniteUnique = require('./sortedUnion')
+
+describe('Intermediate Algorithm Scripting: Sorted Union', () => {
+  test('uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]) should return [1, 3, 2, 5, 4].', () => {
+    expect(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])).toStrictEqual([
+      1,
+      3,
+      2,
+      5,
+      4,
+    ])
+  })
+
+  test('uniteUnique([1, 2, 3], [5, 2, 1]) should return [1, 2, 3, 5]', () => {
+    expect(uniteUnique([1, 2, 3], [5, 2, 1])).toStrictEqual([1, 2, 3, 5])
+  })
+
+  test('uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]) should return [1, 2, 3, 5, 4, 6, 7, 8].', () => {
+    expect(
+      uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])
+    ).toStrictEqual([1, 2, 3, 5, 4, 6, 7, 8])
+  })
+})
